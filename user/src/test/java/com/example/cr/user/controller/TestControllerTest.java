7287b5f56f4475a95247822f1b6f31d60059d91b
GuoXuanKun <1925918787@qq.com>
2025-01-17 20:46:53 +0800

    test: 增加 TestControllerTest 单元测试

---
+++ user/src/test/java/com/example/cr/user/controller/TestControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.cr.user.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void test_shouldReturnExpectedContent() throws Exception {
+        mockMvc.perform(get("/test"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("test"));
+    }
+
+}
