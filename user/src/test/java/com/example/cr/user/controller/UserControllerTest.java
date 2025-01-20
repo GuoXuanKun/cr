587cd881cdfead46338f7c15b11433b0d37d58ff
GuoXuanKun <1925918787@qq.com>
2025-01-20 23:13:22 +0800

    test: 增加 UserController 的单元测试

---
+++ user/src/test/java/com/example/cr/user/controller/UserControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.cr.user.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testCount() throws Exception {
+        mockMvc.perform(get("/count"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"));
+    }
+}
