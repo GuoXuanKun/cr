bbe217d9a5f437f2dc35dd2dc4ff414622cce272
GuoXuanKun <1925918787@qq.com>
2025-01-21 14:05:06 +0800

    test: 单元测试改用独立的数据库
    增加 application-test.xml 配置文件
    测试类上增加 @ActiveProfiles("test") 注解

--- user/src/test/java/com/example/cr/user/controller/UserControllerTest.java
+++ user/src/test/java/com/example/cr/user/controller/UserControllerTest.java
@@ -1,5 +1,9 @@
 package com.example.cr.user.controller;
 
+import com.example.cr.user.entity.User;
+import com.example.cr.user.mapper.UserMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,10 +21,40 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 在每个测试方法执行之前 , 清空用户数据表
+     */
+    @BeforeEach
+    public void beforeTest(){
+        userMapper.deleteByExample(null);
+    }
+
+    /**
+     * 在每个测试方法执行之后 , 清空用户数据表
+     */
+    @AfterEach
+    public void afterTest(){
+        userMapper.deleteByExample(null);
+    }
+
+    /**
+     * 测试查询用户数量
+     * @throws Exception
+     */
     @Test
     public void testCount() throws Exception {
+        long expected = 10L;
+        for (int i = 0; i < expected; i++) {
+            User user = new User();
+            user.setId(Long.parseLong(""+i));
+            user.setMobile("1234567890" + i) ;
+            userMapper.insert(user);
+        }
         mockMvc.perform(get("/count"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("1"));
+                .andExpect(content().string("expected"));
     }
 }
