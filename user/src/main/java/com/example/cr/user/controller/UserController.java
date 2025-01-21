1fc194686978f63ba9569b2990a048b8ecba4964
GuoXuanKun <1925918787@qq.com>
2025-01-21 19:47:15 +0800

    feat: 添加最基础的用户注册功能

--- user/src/main/java/com/example/cr/user/controller/UserController.java
+++ user/src/main/java/com/example/cr/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.cr.user.controller;
 import com.example.cr.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +15,11 @@ public class UserController {
     @GetMapping("/count")
     public long count() {
         return userService.count();
+    }
+
+    @PostMapping("/register")
+    public int register(String mobile) {
+        return userService.register(mobile);
     }
 
 
--- user/src/main/java/com/example/cr/user/service/UserService.java
+++ user/src/main/java/com/example/cr/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.cr.user.service;
 
+import com.example.cr.user.entity.User;
 import com.example.cr.user.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,18 @@ public class UserService {
     private UserMapper userMapper;
     public long count() {
         return userMapper.countByExample(null);
+    }
+
+    /**
+     * 用户注册
+     * @param mobile
+     * @return
+     */
+    public int register(String mobile){
+        User user = new User();
+        user.setId(System.currentTimeMillis());
+        user.setMobile(mobile);
+        return userMapper.insert(user);
     }
 
 }
