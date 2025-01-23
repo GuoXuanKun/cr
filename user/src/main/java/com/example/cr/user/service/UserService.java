9878c132619a260687e774c5242e9150b3993a4c
GuoXuanKun <1925918787@qq.com>
2025-01-23 18:48:44 +0800

    feat: 增加验证“手机号是否注册”

--- user/src/main/java/com/example/cr/user/service/UserService.java
+++ user/src/main/java/com/example/cr/user/service/UserService.java
@@ -1,9 +1,12 @@
 package com.example.cr.user.service;
 
 import com.example.cr.user.entity.User;
+import com.example.cr.user.entity.UserExample;
 import com.example.cr.user.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -20,6 +23,15 @@ public class UserService {
      * @return
      */
     public int register(String mobile){
+
+        // 校验手机号是否已经注册
+        UserExample example = new UserExample();
+        example.createCriteria().andMobileEqualTo(mobile);
+        List<User> users = userMapper.selectByExample(example);
+        if (!users.isEmpty()) {
+            throw new RuntimeException("手机号已经注册");
+        }
+
         User user = new User();
         user.setId(System.currentTimeMillis());
         user.setMobile(mobile);
