3a8c5f2c117c7fafb263efb51103e01216b92329
GuoXuanKun <1925918787@qq.com>
2025-01-24 11:16:40 +0800

    refactor: 自定义校验描述 @NotBlank(message = "手机号不能为空")

--- user/src/main/java/com/example/cr/user/dto/UserDTO.java
+++ user/src/main/java/com/example/cr/user/dto/UserDTO.java
@@ -1,10 +1,12 @@
 package com.example.cr.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class UserDTO {
 
     @NotBlank(message = "手机号不能为空")
+    @Size(min = 11, max = 11, message = "手机号长度必须为11位")
     private String mobile;
 
     public String getMobile() {
