ff448c99317cdf8e3f62e6b047d371cf76c35ff1
GuoXuanKun <1925918787@qq.com>
2025-01-24 11:18:54 +0800

    feat: 增加校验：手机号格式不正确
    这条正则表达式规则已经包含了之前的 "11位" 规则，为了知道有 @Size 注解，先不删它了

--- user/src/main/java/com/example/cr/user/dto/UserDTO.java
+++ user/src/main/java/com/example/cr/user/dto/UserDTO.java
@@ -1,12 +1,14 @@
 package com.example.cr.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class UserDTO {
 
     @NotBlank(message = "手机号不能为空")
     @Size(min = 11, max = 11, message = "手机号长度必须为11位")
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
     private String mobile;
 
     public String getMobile() {
