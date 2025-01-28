dec4311a3b365c39fcb7255e79fb97abb84e0e42
GuoXuanKun <1925918787@qq.com>
2025-01-29 00:55:14 +0800

    feat(web): Pinia store 结合 localStorage 保存或清理登录用户信息
    1. 从 localStorage 读取数据
    2. 在 setUserInfo 时同时将数据保存到 localStorage
    3. 在 clearUserInfo 时同时清除 localStorage 中的数据
    
    现在即使刷新页面，用户信息也不会丢失，直到用户主动登出或清除浏览器数据

--- web/src/stores/user.js
+++ web/src/stores/user.js
@@ -2,20 +2,28 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useUserStore = defineStore('user', () => {
-  const id = ref('')
-  const mobile = ref('')
-  const token = ref('')
+  const id = ref(localStorage.getItem('userId') || '')
+  const mobile = ref(localStorage.getItem('userMobile') || '')
+  const token = ref(localStorage.getItem('userToken') || '')
 
   const setUserInfo = (userInfo) => {
     id.value = userInfo.id
     mobile.value = userInfo.mobile
     token.value = userInfo.token
+
+    localStorage.setItem('userId', id.value);
+    localStorage.setItem('userMobile', mobile.value);
+    localStorage.setItem('userToken', token.value);
   }
 
   const clearUserInfo = () => {
     id.value = ''
     mobile.value = ''
     token.value = ''
+
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userMobile');
+    localStorage.removeItem('userToken');
   }
 
   return {
@@ -25,4 +33,4 @@ export const useUserStore = defineStore('user', () => {
     setUserInfo,
     clearUserInfo
   }
-}) 
+})
