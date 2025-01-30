16a7afc62709fcc3df711da8e957818df4f35dfb
GuoXuanKun <1925918787@qq.com>
2025-01-30 22:29:34 +0800

    chore: 创建乘车人表

--- sql/cr_user.sql
+++ sql/cr_user.sql
@@ -1,3 +1,4 @@
+-- 用户/会员
 drop table if exists `cr_user`;
 
 create table `cr_user`
@@ -7,3 +8,20 @@ create table `cr_user`
     primary key (`id`),
     unique key `mobile_unique` (`mobile`)
 ) default charset = utf8mb4 comment ='会员';
+
+
+-- 乘车人/乘客
+-- 乘车人不一定是网站的会员。比如你给爷爷奶奶等买票，他们只是单纯的乘车人身份
+drop table if exists `cr_passenger`;
+create table `cr_passenger`
+(
+    `id`          bigint      not null comment 'id',
+    `user_id`   bigint      not null comment '会员id',
+    `name`        varchar(20) not null comment '姓名',
+    `id_card`     varchar(18) not null comment '身份证号',
+    `type`        char(1)     not null comment '旅客类型',
+    `create_time` datetime(3) not null comment '创建时间',
+    `update_time` datetime(3) not null comment '更新时间',
+    primary key (`id`),
+    key `index_user_id` (`user_id`)
+) default charset = utf8mb4 comment ='乘车人';
